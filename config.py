7f345473f9e0f12e05a9fd766696ddb712da878b
Andrei <fmwar92@gmail.com>
2022-10-09 23:46:23 +0300

    delete token and id

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 settings = {
-    'discord_token': 'MTAyODMyODQ0NTc0NDI1MTAxMQ.GB2KNw.xU0cfnmNa2XrScyd-oPgQulpLY5pef9Kj0Lfuk',
+    'discord_token': '',
     'bot': 'bot_sheriff',
-    'discord_id': 1026919368543715339,
+    'discord_id': ,
     'prefix': '!'
 }
