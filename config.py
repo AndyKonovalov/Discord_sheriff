0d3ac2771df379857ef15f027c8c610edd860cbb
Andrei <fmwar92@gmail.com>
2022-10-09 15:13:45 +0300

    added config

---
+++ config.py
@@ -0,0 +1,8 @@
+settings = {
+    'discord_token': '',
+    'bot': 'bot_sheriff',
+    'discord_id': 1026919368543715339,
+    'reddit_id': 'yolPV_SmnjwDfuNUy0eFCw',
+    'reddit_code': 'M1ULcC1g8BTF-x32faBq_VWhPLAQgg',
+    'prefix': '!'
+}
